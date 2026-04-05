552e50cb91c8f50e06daaec5eb664707624bd96f
Soudip Panja <soudip03panja@gmail.com>
2026-04-06 00:47:15 +0530

    fix: env.js fix

--- config/env.js
+++ config/env.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  SECRET_KEY: process.env.SECRET_KEY,
   JWT_SECRET: process.env.JWT_SECRET,
-  EMAIL: process.env.EMAIL,
   PORT: process.env.PORT || 3000,
 };
