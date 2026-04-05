a46ee64b13a03b6169bcfc15121f9c299b945da2
Soudip Panja <soudip03panja@gmail.com>
2026-04-05 21:49:16 +0530

    feat: Update finance record route added

--- routes/financeRecord.routes.js
+++ routes/financeRecord.routes.js
@@ -51,7 +51,7 @@ async function getAllRecords() {
   try {
     const records = await FinancialRecord.find().populate(
       "recordBy",
-      "name email role"
+      "name email role",
     );
 
     return records;
@@ -64,6 +64,35 @@ async function getAllRecords() {
 router.get("/records", async (req, res) => {
   try {
     const data = await getAllRecords();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update record
+async function updateRecord(recordId, updatedData) {
+  try {
+    const updatedRecord = await FinancialRecord.findByIdAndUpdate(
+      recordId,
+      updatedData,
+      { returnDocument: "after", runValidators: true },
+    ).populate("recordBy", "name email role");
+
+    if (!updatedRecord) {
+      throw new Error(`Record with ID ${recordId} not found.`);
+    }
+
+    return updatedRecord;
+  } catch (error) {
+    console.log("Error updating record.", error.message);
+    throw error;
+  }
+}
+
+router.post("/records/:id", async (req, res) => {
+  try {
+    const data = await updateRecord(req.params.id, req.body);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
