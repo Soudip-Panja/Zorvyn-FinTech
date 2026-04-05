8d7c979e7b1e6c97ab7a71747ffac4d6617afba8
Soudip Panja <soudip03panja@gmail.com>
2026-04-05 08:37:51 +0530

    feat: created a simple protected route

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
 const SECRET_KEY = process.env.SECRET_KEY;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const verifyJWT = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+  try {
+    // console.log(token);
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(402).json({ message: "Invalid Token." });
+  }
+};
 
 app.post("/admin/login", (req, res) => {
   const { secret } = req.body;
   if (secret === SECRET_KEY) {
-    res.json({ message: "Access Granted" });
+    const token = jwt.sign({ role: "admin" }, JWT_SECRET, { expiresIn: "24h" });
+    res.json({ token });
   } else {
     res.json({ message: "Invalid Secret" });
   }
+});
+
+app.get("/admin/api/data", verifyJWT, (req, res) => {
+  res.json({ message: "Protected route accessable" });
 });
 
 const PORT = process.env.PORT || 3000;
